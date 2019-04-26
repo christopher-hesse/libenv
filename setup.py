2a23e05b4e1c9d43afcf83653efc3647cd99d9bd
Christopher Hesse <48501609+cshesse@users.noreply.github.com>
2019-04-25 23:05:12 -0700

    csh_test_envs

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="libenv",
     version="0.1",
     packages=find_packages(),
-    install_requires=["gym~=0.10", "cffi~=1.11", "numpy~=1.14"],
+    install_requires=["gym~=0.10.0", "cffi~=1.11", "numpy~=1.14"],
     extras_require={"dev": ["pytest", "pytest-benchmark"]},
     use_scm_version=False
     if os.environ.get("USE_SCM_VERSION", "1") == "0"

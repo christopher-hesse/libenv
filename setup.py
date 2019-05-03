bd6aae23f1fc8541d475b5305426bf8bfffd434b
Christopher Hesse <48501609+cshesse@users.noreply.github.com>
2019-05-02 23:06:21 -0700

    setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,21 @@ import os
 from setuptools import setup, find_packages
 
 
-setup(
+setup_dict = dict(
     name="libenv",
     version="0.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["gym~=0.12.1", "cffi~=1.11", "numpy~=1.14"],
     extras_require={"dev": ["pytest", "pytest-benchmark"]},
-    use_scm_version=False
-    if os.environ.get("USE_SCM_VERSION", "1") == "0"
-    else {"root": "..", "relative_to": __file__},
-    setup_requires=["setuptools_scm"],
 )
+
+if os.environ.get("USE_SCM_VERSION", "1") == "1":
+    setup_dict["use_scm_version"] = {
+        "root": "..",
+        "relative_to": __file__,
+        "local_scheme": "node-and-timestamp",
+    }
+    setup_dict["setup_requires"] = ["setuptools_scm"]
+
+setup(**setup_dict)
